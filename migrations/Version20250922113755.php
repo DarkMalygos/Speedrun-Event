25a676e9672abe16e9975ae53b354248ed53f7d2
DarkMalygos <krisztofer.feher@gmail.com>
2025-09-22 13:58:27 +0200

    updated files for deployment

---
+++ migrations/Version20250922113755.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250922113755 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE registration (id INT AUTO_INCREMENT NOT NULL, event_id INT DEFAULT 1 NOT NULL, spectator_id INT NOT NULL, waitlist_position INT DEFAULT NULL, status VARCHAR(255) NOT NULL, UNIQUE INDEX unique_registration (event_id, spectator_id), INDEX IDX_62A8A7A771F7E88B (event_id), INDEX IDX_62A8A7A7523FB688 (spectator_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE run_event (id INT DEFAULT 1 NOT NULL, run_name VARCHAR(70) NOT NULL, capacity INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE spectator (id INT AUTO_INCREMENT NOT NULL, spectator_name VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UQ_email (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE run_event ADD CONSTRAINT run_event_only_one_row CHECK (id = 1)');
+        $this->addSql('ALTER TABLE registration ADD CONSTRAINT FK_62A8A7A771F7E88B FOREIGN KEY (event_id) REFERENCES run_event (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE registration ADD CONSTRAINT FK_62A8A7A7523FB688 FOREIGN KEY (spectator_id) REFERENCES spectator (id) ON DELETE CASCADE');
+        $this->addSql('INSERT INTO run_event (id, run_name, capacity) VALUES (1, "Jak and Daxter: The Precursor Legacy", 5)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE registration DROP FOREIGN KEY FK_62A8A7A771F7E88B');
+        $this->addSql('ALTER TABLE registration DROP FOREIGN KEY FK_62A8A7A7523FB688');
+        $this->addSql('DROP TABLE registration');
+        $this->addSql('DROP TABLE run_event');
+        $this->addSql('DROP TABLE spectator');
+        $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('DELETE FROM run_event WHERE id = 1');
+    }
+}
